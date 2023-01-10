865ce8fe70048237d6940bd5bdea2524fe605993
PSM <gkdisrha3920@naver.com>
2023-01-10 23:43:43 +0900

    프로젝트 첫 커밋

---
+++ open.js
@@ -0,0 +1,7 @@
+const text1 = document.querySelector('#text1');
+
+
+// 경기도 축구장 지역 좌표 가져옴 
+const URL = "https://openapi.gg.go.kr/PublicTrainingFacilitySoccer";
+
+fetch(URL).then(Response() = JSON.Response)
