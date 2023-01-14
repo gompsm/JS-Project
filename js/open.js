86c713105bfd155a36372dfd3d5944c0aac103f5
PSM <gkdisrha3920@naver.com>
2023-01-14 23:10:20 +0900

    JSON 테스트

--- js/open.js
+++ js/open.js
@@ -2,6 +2,13 @@ const text1 = document.querySelector('#text1');
 
 
 // 경기도 축구장 지역 좌표 가져옴 
-const URL = "https://openapi.gg.go.kr/PublicTrainingFacilitySoccer";
+const URL = 'https://openapi.gg.go.kr/PublicTrainingFacilitySoccer?KEY=8a290b0ff1194294b392beff3ab0b325&Type=json&pindex=1&psize=100';
 
-fetch(URL).then(Response() = JSON.Response)
+
+let ha = fetch(URL)
+  .then((response) => response.json())
+  .then((data) => console.log(data.PublicTrainingFacilitySoccer[1].row[0]));
+
+
+const li = document.querySelector('li');
+
